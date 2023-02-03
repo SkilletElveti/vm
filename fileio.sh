f411b4989897f3ef7c46b5e52a37dd6c0ea1c9f7
Shubham Kamdi <36183674+SkilletElveti@users.noreply.github.com>
2023-02-02 23:37:06 -0800

    Update fileio.sh

--- fileio.sh
+++ fileio.sh
@@ -7,8 +7,22 @@
 #  
 for i in {1..5}
 do
-    sysbench fileio --file-total-size=1G prepare
-    sysbench fileio --file-total-size=1G --file-test-mode=rndrw --time=300 --threads=8 run
-    sysbench fileio --file-total-size=1G cleanup
+    sysbench fileio --file-total-size=$file_size prepare
+    sysbench fileio --file-total-size=$file_size --file-test-mode=rndrw --time=30 --threads=1 run
+    sysbench fileio --file-total-size=$file_size cleanup
+done
+
+for i in {1..5}
+do
+    sysbench fileio --file-total-size=2G prepare
+    sysbench fileio --file-total-size=2G --file-test-mode=rndrw --time=30 --threads=1 run
+    sysbench fileio --file-total-size=2G cleanup
+done
+
+for i in {1..5}
+do
+    sysbench fileio --file-total-size=3G prepare
+    sysbench fileio --file-total-size=3G --file-test-mode=rndrw --time=30 --threads=1 run
+    sysbench fileio --file-total-size=3G cleanup
 done
 
