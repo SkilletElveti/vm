fcfcdfc6e578086969cda729edac2f97248fddd4
Shubham Kamdi <36183674+SkilletElveti@users.noreply.github.com>
2023-02-03 22:00:31 -0800

    Update fileio.sh

--- fileio.sh
+++ fileio.sh
@@ -5,13 +5,13 @@
 #
 #  Created by Shubham Kamdi on 2/2/23.
 #  
-file_size=3G
+file_size=1G
 
 # Prepare the test file
 sysbench fileio --file-total-size=$file_size prepare
 
 # Run the read test
-sysbench fileio --file-total-size=$file_size --file-test-mode=rndrw --time=30 --threads=2 run
+sysbench fileio --file-total-size=$file_size --file-test-mode=rndrw --time=30 --threads=1 run
 
 # Clean up the test file
 sysbench fileio --file-total-size=$file_size cleanup
